f3ecacfdbbf47132e41c84b1cbd5670c4c7e8301
Andrei Kulakov <kulakov1@tut.by>
2023-04-21 23:30:37 +0300

    poetry fix v2 by https://community.render.com/t/update-poetry-to-1-2-0/7957/19

--- build.sh
+++ build.sh
@@ -3,18 +3,17 @@
 # exit on error
 set -o errexit
 
-echo "Creating a virtual env for both poetry and our packages..."
-python -m venv venv
+echo "Installing the latest version of poetry..."
 
-echo "Updating pip ;-)..."
-./venv/bin/python -m pip install --upgrade pip
+pip install --upgrade pip
 
-echo "Installing new/better version of poetry into our virtual env..."
-./venv/bin/pip install poetry==1.4.2
+pip install poetry==1.4.2
 
-echo "Installing our (non-dev) packages..."
-cd /opt/render/project/src
-./venv/bin/poetry install --without dev
+rm poetry.lock
+
+poetry lock
+
+python -m poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
