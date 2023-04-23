23ee099714a50386df9fb464b7af32ec012c96dc
Andrei Kulakov <kulakov1@tut.by>
2023-04-23 22:57:31 +0300

    fix admin

--- cars/admin.py
+++ cars/admin.py
@@ -5,7 +5,7 @@ from .models import Car
 class CarAdmin(admin.ModelAdmin):
     list_display = ('brand', 'model', 'year', 'engine', 'gearbox', 'vin', 'is_hidden', 'is_hidden_v2')
     search_fields = ('brand', 'model', 'vin')
-    ordering = '-id'
+    ordering = ('-id', )
 
 
 admin.site.register(Car, CarAdmin)
